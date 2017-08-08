a135aa3bcb4f3276a7be52a9e42275c557cf9183
suechan <suechan@wweco.com>
2017-08-08 18:43:03 +0800

    completed API test

---
+++ test/login.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// library requires
+var request = require('supertest')
+var chai = require("chai");
+var expect = chai.expect;
+
+var app = "https://reqres.in/api";
+
+describe("POST /login", function () {
+    it("should login successfully with valid email and password", function () {
+        request(app).post("/login").send({ email: "peter@klaven", password: "cityslicka" })
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(function (res) {
+                expect(res.body.token).eq("QpwL5tke4Pnpja7X");
+
+            })
+            .expect(200);
+    });
+
+    it("should fail without password", function (done) {
+        request(app).post("/login").send({ email: "peter@klaven" })
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(function (res) {
+                expect(res.body.error).eq("Missing password");
+            })
+            .expect(400,done);
+
+    });
+});
+
---
+++ test/user.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// library requires
+var request = require('supertest')
+var chai = require("chai");
+var expect = chai.expect;
+
+var app = "https://reqres.in/api";
+
+describe("GET /user", function () {
+  it("should get status 200", function () {
+    return request(app).get("/users").expect(200);
+  })
+
+  it("should return correct keys in user list", function (done) {
+    request(app).get("/users?page=2")
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .expect(function (res) {
+        expect(res.body.page).eq("2");
+        expect(res.body.per_page).eq(3);
+        res.body.data.forEach(function (element) {
+          expect(element).to.have.property('id');
+          expect(element).to.have.property('first_name');
+          expect(element).to.have.property('last_name');
+          expect(element).to.have.property('avatar');
+
+        });
+
+      }).expect(200, done);
+  })
+
+  it("should get correct data from a specific user ", function (done) {
+
+    request(app).get("/users/2").expect(function (res) {
+      expect(res.body.data.id).eq(2);
+      expect(res.body.data.first_name).eq("lucille");
+      expect(res.body.data.last_name).eq("bluth");
+      expect(res.body.data.avatar).eq("https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg");
+
+    }) .expect(200, done);
+  })
+});
+
+
+
